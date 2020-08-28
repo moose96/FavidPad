53f330e64585a4fd5f359b93c4d1ee50a08402c4
Piotr Łosiak <piotr.losiak96@gmail.com>
2020-08-28 23:28:55 +0200

    add third api entry point and fix scroll issue

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 // export const API_URL = 'http://localhost:3000/v1';
-export const API_URL = 'http://srapi.herokuapp.com/v1';
+// export const API_URL = 'http://srapi.herokuapp.com/v1';
+export const API_URL = 'https://favidpad-api.herokuapp.com/v1';
 
 function request(url, method = 'GET', data = null) {
   const config = {
